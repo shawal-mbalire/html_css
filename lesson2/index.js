d1c4cb4670663fbd8e1671753f7e349e356ec561
Shawal Mbalire <mbalireshawal@gmail.com>
2023-07-05 16:51:26 +0300

    lesson2 a simple calculator application

---
+++ lesson2/index.js
@@ -0,0 +1,13 @@
+// We have string variable with name str
+var input = [];
+var inputs_list = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', '.','+','-','*','/']
+
+var output=getOutput(input.join(''));
+
+// We have string input which we must evaluate
+function getOutput(input_String){ // 4567.7*9+3-9
+    // your code here
+
+
+    return ;
+}
